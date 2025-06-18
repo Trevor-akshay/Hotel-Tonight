77b0e9d8e497a88a14b9aefa173d963c4f67efed
Trevor-akshay <trevorakshay@gmail.com>
2025-06-18 22:31:06 +0530

    Updated to MongoDB cloud atlas

--- server/index.mjs
+++ server/index.mjs
@@ -1,31 +1,37 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
-import dotenv from 'dotenv';
-dotenv.config({ path: './configs/.env' });
+import dotenv from "dotenv";
+dotenv.config({ path: "./configs/.env" });
 
-import createConnection from './configs/mongodbConnection.mjs';
-import userRouter from './routes/user.mjs';
-import hotelRouter from './routes/hotel.mjs';
-import roomRouter from './routes/room.mjs';
+import createConnection from "./configs/mongodbConnection.mjs";
+import userRouter from "./routes/user.mjs";
+import hotelRouter from "./routes/hotel.mjs";
+import roomRouter from "./routes/room.mjs";
 
 const app = express();
 
-createConnection()
-	.then(() => {
-		app.use(cookieParser());
-		app.use(cors());
-		app.use(bodyParser.json());
-		app.use('/users', userRouter);
-		app.use('/hotels', hotelRouter);
-		app.use('/rooms', roomRouter);
+const PORT = process.env.PORT;
 
-		app.listen(process.env.PORT, () => {
-			console.log(`Server started @${process.env.PORT}`);
-		});
-	})
-	.catch((err) => {
-		console.log(err.message);
-	});
+if (!PORT) {
+  throw new Error("PORT is not defined in the environment");
+}
+
+createConnection()
+  .then(() => {
+    app.use(cookieParser());
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use("/users", userRouter);
+    app.use("/hotels", hotelRouter);
+    app.use("/rooms", roomRouter);
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server started @${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
--- server/services/user.mjs
+++ server/services/user.mjs
@@ -1,83 +1,82 @@
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import userSchema from '../models/user.mjs';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import userSchema from "../models/user.mjs";
 
-import dotenv from 'dotenv';
-dotenv.config({ path: './configs/.env' });
+import dotenv from "dotenv";
+dotenv.config({ path: "./configs/.env" });
 
 export const postUserService = async (body) => {
-	try {
-		const user = new userSchema(body);
-		await user.save();
-		const { isAdmin, password, ...otherDetails } = user._doc;
-		return { ...otherDetails }
-	} catch (error) {
-		throw error;
-	}
+  try {
+    const user = new userSchema(body);
+    await user.save();
+    const { isAdmin, password, ...otherDetails } = user._doc;
+    return { ...otherDetails };
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const loginUserService = async (body) => {
-	try {
-		const user = await userSchema.findOne({ emailId: body.emailId });
-		if (!user) throw new Error(`${body.emailId} not found`);
+  try {
+    const user = await userSchema.findOne({ emailId: body.emailId });
+    if (!user) throw new Error(`${body.emailId} not found`);
 
-		const isPasswordCorrect = await bcrypt.compare(
-			body.password,
-			user.password
-		);
-		if (!isPasswordCorrect) throw new Error(`Wrong password`);
-		const content = {
-			id: user._id,
-			email: user.emailId,
-			isAdmin : user.isAdmin
-		};
-		return jwt.sign(content, process.env.JWT_KEY);
-	} catch (error) {
-		console.log('here')
-		throw error;
-	}
+    const isPasswordCorrect = await bcrypt.compare(
+      body.password,
+      user.password
+    );
+    if (!isPasswordCorrect) throw new Error(`Wrong password`);
+    const content = {
+      id: user._id,
+      email: user.emailId,
+      isAdmin: user.isAdmin,
+    };
+    return jwt.sign(content, process.env.JWT_KEY);
+  } catch (error) {
+    throw error;
+  }
 };
 export const getUsersService = async () => {
-	try {
-		return await userSchema.find();
-	} catch (error) {
-		throw error;
-	}
+  try {
+    return await userSchema.find();
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const getUserByIdService = async (id) => {
-	try {
-
-		const user = await userSchema.find({ _id: id });
-		const { isAdmin, password, ...otherDetails } = user._doc;
-		return { ...otherDetails }
-	} catch (error) {
-		throw error;
-	}
+  try {
+    const user = await userSchema.findById(id);
+    console.log(user._doc);
+    const { isAdmin, password, ...otherDetails } = user._doc;
+    return { ...otherDetails };
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const updateUserService = async (id, body) => {
-	try {
-		const user = await userSchema.findByIdAndUpdate(
-			{ _id: id },
-			{
-				$set: body,
-			},
-			{ new: true }
-		);
-		const { isAdmin, password, ...otherDetails } = user._doc;
-		return { ...otherDetails }
-	} catch (error) {
-		throw error;
-	}
+  try {
+    const user = await userSchema.findByIdAndUpdate(
+      { _id: id },
+      {
+        $set: body,
+      },
+      { new: true }
+    );
+    const { isAdmin, password, ...otherDetails } = user._doc;
+    return { ...otherDetails };
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const deleteUserService = async (id) => {
-	try {
-		const user = await userSchema.findByIdAndDelete({ _id: id });
-		const { isAdmin, password, ...otherDetails } = user._doc;
-		return { ...otherDetails }
-	} catch (error) {
-		throw error;
-	}
+  try {
+    const user = await userSchema.findByIdAndDelete({ _id: id });
+    const { isAdmin, password, ...otherDetails } = user._doc;
+    return { ...otherDetails };
+  } catch (error) {
+    throw error;
+  }
 };
